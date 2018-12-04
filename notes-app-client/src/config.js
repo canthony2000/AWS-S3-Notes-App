65807a8ffea55af543c00fae131f7fb0a35c2136
Corey Anthony <canthony@techtonic.com>
2018-12-04 11:13:47 -0700

    Initial commit at working login page.

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "canthony1"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://5l143cfg63.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_F8j1cmCHZ",
+    APP_CLIENT_ID: "4pa9p9e0eiaefrslrrd84d5d74",
+    IDENTITY_POOL_ID: "us-east-2:6b57144b-7870-4263-bf11-f0a9d93f9d1c"
+  }
+};
