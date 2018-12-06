d341baef14878b74a1b8d2f5fef37d5bfa67a40a
Corey Anthony <canthony@techtonic.com>
2018-12-06 10:59:42 -0700

    Add build script.

--- notes-app-client/src/components/AuthenticatedRoute.js
+++ notes-app-client/src/components/AuthenticatedRoute.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export default ({ component: C, props: cProps, ...rest }) =>
-  <Route
-    {...rest}
-    render={props =>
-      cProps.isAuthenticated
-        ? <C {...props} {...cProps} />
-        : <Redirect
-          to={`/login?redirect=${props.location.pathname}${props.location
-            .search}`}
-        />}
-  />;
+export default ({ component: C, props: cProps, ...rest }) => {
+  console.log("hello", rest)
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        cProps.isAuthenticated
+          ? <C {...props} {...cProps} />
+          : <Redirect
+            to={`/login?redirect=${props.location.pathname}${props.location
+              .search}`}
+          />}
+    />
+  );
+}
